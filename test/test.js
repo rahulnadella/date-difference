4c91cae82570f2047b42daee9fc0c8bad430ddb4
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-27 15:30:43 -0400

    Added test folder

---
+++ test/test.js
@@ -0,0 +1,15 @@
+'use strict'
+var assert = require('assert');
+var dateDifference = require('../');
+
+describe('date-difference', function(){
+  describe('#dateDifferenceByDays()', function(){
+    it('returns the difference between dates in milliseconds', function()
+    {
+      var date1 = new Date("5/27/2014");
+      var date2 = new Date("5/27/2015");
+
+      assert.equal(dateDifference(date1, date2), '365');
+    })
+  })
+});
