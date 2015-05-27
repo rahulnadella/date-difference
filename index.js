7ea7a3bc95b769d58ac369e9d6cdfc75ddfead42
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-27 17:04:30 -0400

    Updated index.js to take an additional optional parameter

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict'
 var prettyMs = require('pretty-ms')
 
-module.exports = function(a, b)
+/*
+Returns the difference between two dates representated in a specific format
+*/
+module.exports = function(a, b, opts)
 {
   var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
   var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
 
-  return prettyMs(utc2 - utc1);
+  return prettyMs(utc2 - utc1, opts);
 }
