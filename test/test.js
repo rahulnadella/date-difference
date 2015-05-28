333e5325f3b97c92f02a320679d121d93ead2dad
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-28 13:18:38 -0400

    Updated test case

--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@ var assert = require('assert');
 var dateDifference = require('../');
 
 describe('date-difference', function(){
-  describe('#dateDifferenceByDays()', function(){
-    it('returns the difference between dates in milliseconds', function()
+  describe('#dateDifferenceWithoutOptions()', function(){
+    it('returns the difference between the two dates', function()
     {
-      var date1 = new Date("5/27/2014");
-      var date2 = new Date("5/27/2015");
+      var date1 = new Date("5/10/2014");
+      var date2 = new Date("5/10/2015");
 
       assert.equal(dateDifference(date1, date2), '1y');
 
@@ -20,6 +20,26 @@ describe('date-difference', function(){
       date2 = new Date("2015-05-27T07:08:07");
 
       assert.equal(dateDifference(date1, date2), '34y 338d 4h 8m 7s');
+    })
+  })
+
+  describe('#dateDifferenceWithOptions', function(){
+    it('returns the difference between the two dates with specified options', function()
+    {
+      var date1 = new Date("2014-05-26T03:00:00");
+      var date2 = new Date("2015-05-27T07:08:07");
+
+      assert.equal(dateDifference(date1, date2, {compact: true}), '~1y');
+
+      date1 = new Date("2015-01-25T12:00:00");
+      date2 = new Date("2015-03-05T04:01:23");
+
+      assert.equal(dateDifference(date1, date2, {compact: true}), '~38d');
+
+      date1 = new Date("2015-04-25T12:00:00");
+      date2 = new Date("2015-04-25T16:01:23");
+
+      assert.equal(dateDifference(date1, date2, {compact: true}), '~4h');
     })
   })
 });
