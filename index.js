a754431b229274150ba6ac6dfd2e93d82e6fb78a
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-27 20:08:26 -0400

    Added more functions to index.js

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 'use strict'
-var prettyMs = require('pretty-ms')
+var prettyMS = require('pretty-ms');
+
+function dateOutput(ms)
+{
+  if (typeof ms !== 'number')
+  {
+    throw new TypeError('Expected a number');
+  }
+
+  return {
+    years: Math.floor(ms / 31536000000),
+    days: Math.floor(ms / 86400000 % 365),
+		hours: Math.floor(ms / 3600000 % 24),
+		minutes: Math.floor(ms / 60000 % 60),
+		seconds: Math.floor(ms / 1000 % 60),
+		milliseconds: Math.floor(ms % 1000)
+  };
+}
+
+function add(key, value, postfix)
+{
+	if (value > 0)
+  {
+		key.push(value + postfix);
+	}
+
+	return key;
+}
+
+function datePrettyFormat(ms, opts) {
+	if (typeof ms !== 'number')
+  {
+		throw new TypeError('Expected a number');
+	}
+
+	if (ms < 1000)
+  {
+		return Math.ceil(ms) + 'ms';
+	}
+
+	opts = opts || {};
+
+	var secDecimalDigits = typeof opts.secDecimalDigits === 'number' ? opts.secDecimalDigits : 1;
+	var ret = [];
+	var parsed = dateOutput(ms);
+
+  ret = add(ret, parsed.years, 'y');
+	ret = add(ret, parsed.days, 'd');
+	ret = add(ret, parsed.hours, 'h');
+	ret = add(ret, parsed.minutes, 'm');
+
+	if (opts.compact)
+  {
+		ret = add(ret, parsed.seconds, 's');
+		return '~' + ret[0];
+	}
+
+	ret = add(ret, (ms / 1000 % 60).toFixed(secDecimalDigits).replace(/\.0$/, ''), 's');
+
+	return ret.join(' ');
+};
 
 /*
-Returns the difference between two dates representated in a specific format
+Most of the solutions don't take into account a case that fails when the two dates involved go across a daylight saving change. In this case, the date on which day light saving change happens will have a duration in milliseconds which != 1000*60*60*24, so the typical calculation will fail.
+
+This function returns the difference between two dates representated in a specific format
 */
 module.exports = function(a, b, opts)
 {
-  var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+  // UTC time never observes Daylight Savings Time (DST)
+  var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate(), a.getHours(), a.getMinutes(), a.getSeconds(), a.getMilliseconds());
+  var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate(), b.getHours(), b.getMinutes(), b.getSeconds(), b.getMilliseconds());
 
-  return prettyMs(utc2 - utc1, opts);
+  return datePrettyFormat((utc2 - utc1), opts);
 }
--- test/test.js
+++ test/test.js
@@ -9,7 +9,17 @@ describe('date-difference', function(){
       var date1 = new Date("5/27/2014");
       var date2 = new Date("5/27/2015");
 
-      assert.equal(dateDifference(date1, date2), '365d');
+      assert.equal(dateDifference(date1, date2), '1y');
+
+      date1 = new Date("2015-01-25T12:00:00");
+      date2 = new Date("2015-03-05T04:01:23");
+
+      assert.equal(dateDifference(date1, date2), '38d 16h 1m 23s');
+
+      date1 = new Date("1980-07-01T03:00:00");
+      date2 = new Date("2015-05-27T07:08:07");
+
+      assert.equal(dateDifference(date1, date2), '34y 338d 4h 8m 7s');
     })
   })
 });
